0ecab4aa84775ed25fa1a7312f0daa5e6880dc38
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-17 18:40:56 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCAfwAjagXYpFvkTvBw3EbgNIIrrOaP0Xc",
+  authDomain: "ctrft-livestream-3e3e7.firebaseapp.com",
+  projectId: "ctrft-livestream-3e3e7",
+  storageBucket: "ctrft-livestream-3e3e7.firebasestorage.app",
+  messagingSenderId: "583221601139",
+  appId: "1:583221601139:web:445d9744d95ca4f3aedb06"
 };
 
 const app = initializeApp(firebaseConfig);
