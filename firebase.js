1627a1d6462dc0cc6416738d1cace9911b7a2dc7
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-30 21:30:37 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,26 +3,23 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBvqmhU5EOiYFQ8N3lSb1SMc7MPCXhOyj0",
-  authDomain: "ctrft-livestream.firebaseapp.com",
-  projectId: "ctrft-livestream",
-  storageBucket: "ctrft-livestream.firebasestorage.app",
-  messagingSenderId: "562371146015",
-  appId: "1:562371146015:web:39a6727864d94684ff402a"
+    apiKey: "AIzaSyBvqmhU5EOiYFQ8N3lSb1SMc7MPCXhOyj0",
+    authDomain: "ctrft-livestream.firebaseapp.com",
+    projectId: "ctrft-livestream",
+    storageBucket: "ctrft-livestream.appspot.com",
+    messagingSenderId: "562371146015",
+    appId: "1:562371146015:web:39a6727864d94684ff402a"
 };
 
-// Initialize Firebase
-let auth;
-try {
-    const app = firebase.initializeApp(firebaseConfig);
-    auth = firebase.auth();
-    console.log('Firebase initialized successfully');
-} catch (error) {
-    console.error('Error initializing Firebase:', error);
-}
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 export { auth };
+
 
 
 
