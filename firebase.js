f8eeafb4cbf4693db17458cd60e3e058ffc34701
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-30 20:46:02 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,7 +23,6 @@ try {
 }
 
 export { auth };
-
 
 
 
