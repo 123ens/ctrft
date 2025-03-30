1a74153a893be4a1c484e06872f7d70bb52c4bb9
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-30 17:52:59 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCAfwAjagXYpFvkTvBw3EbgNIIrrOaP0Xc",
-  authDomain: "ctrft-livestream-3e3e7.firebaseapp.com",
-  projectId: "ctrft-livestream-3e3e7",
-  storageBucket: "ctrft-livestream-3e3e7.firebasestorage.app",
-  messagingSenderId: "583221601139",
-  appId: "1:583221601139:web:445d9744d95ca4f3aedb06"
+onst firebaseConfig = {
+  // Replace these with your Firebase config values
+  apiKey: "AIzaSyBvqmhU5EOiYFQ8N3lSb1SMc7MPCXhOyj0",
+  authDomain: "ctrft-livestream.firebaseapp.com",
+  projectId: "ctrft-livestream",
+  storageBucket: "ctrft-livestream.firebasestorage.app",
+  messagingSenderId: "562371146015",
+  appId: "1:562371146015:web:39a6727864d94684ff402a"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth(); 
 
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-export { auth };
