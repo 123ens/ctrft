a29f7896307e12005a548cd2647ad852a688d4e7
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-30 19:28:48 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -15,6 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
+const auth = getAuth(app);
 
