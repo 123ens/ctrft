dd0b771e16d250b47f4f561d2939aaeabf6c480a
ENSLIN PIETERSE <102644813+123ens@users.noreply.github.com>
2025-03-31 10:09:51 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBvqmhU5EOiYFQ8N3lSb1SMc7MPCXhOyj0",
@@ -12,17 +8,14 @@ const firebaseConfig = {
     appId: "1:562371146015:web:39a6727864d94684ff402a"
 };
 
-let auth;
+// Initialize Firebase
 try {
-    const app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
+    const app = firebase.initializeApp(firebaseConfig);
+    window.auth = firebase.auth();
     console.log('Firebase initialized successfully');
 } catch (error) {
     console.error('Error initializing Firebase:', error);
-}
-
-export { auth };
-
+} 
 
 
 
